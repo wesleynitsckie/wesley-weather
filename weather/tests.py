428350eefc48dc680d431c9570f4d55a2007882d
Wesley Nitsckie <wesley.nitsckie@gometroapp.com>
2020-11-06 08:38:42 +0200

    Adding some unit tests

--- weather/tests.py
+++ weather/tests.py
@@ -3,5 +3,21 @@ from django.test import TestCase
 # Create your tests here.
 class WeatherTest(TestCase):
     
+    def test_index_loads_properly(self):
+        """The index page loads properly"""
+        response = self.client.get('http://localhost:8000')
+        self.assertEqual(response.status_code, 200)
+
     def test_was_requested_succcessfully(self):
-        self.assertIs(1, 1)
+        response = self.client.get('http://localhost:8000/api?location=Pretoria&fromDate=2020-01-01&toDate=2020-01-30')
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        self.assertIs(data['temperature']['max'], 35)
+
+    def test_incorrect_date_format(self):
+        response = self.client.get('http://localhost:8000/api?location=Pretoria&fromDate=2020-01-01&toDate=2020-01-300000')
+        self.assertEqual(response.status_code, 428)
+
+    def test_fail_weather_check(self):
+        response = self.client.get('http://localhost:8000/api?location=asdfasdf&fromDate=2020-01-01&toDate=2020-01-30')
+        self.assertEqual(response.status_code, 500)
